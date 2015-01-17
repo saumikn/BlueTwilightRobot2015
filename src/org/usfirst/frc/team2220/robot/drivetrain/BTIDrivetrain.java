29ad9bb62c9c93afa03cb7375a1478da379af46e
Emerald141 <jacob.elafandi@gmail.com>
2015-01-17 14:52:17 -0600

    Jacob E testing committing abilities

--- src/org/usfirst/frc/team2220/robot/drivetrain/BTIDrivetrain.java
+++ src/org/usfirst/frc/team2220/robot/drivetrain/BTIDrivetrain.java
@@ -1,5 +1,5 @@
 	package org.usfirst.frc.team2220.robot.drivetrain;
-//Test comment by Jacob E. - 1/17/15
+//Test comment by Jacob E. - 1/17/15, 2:51 PM
 public interface BTIDrivetrain
 {
 	public void drive();
