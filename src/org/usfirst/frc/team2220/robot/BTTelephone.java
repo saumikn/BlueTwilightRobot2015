c4f8b292069cef13d522e739d1114c50c9a38069
benpankow <pankow.ben@gmail.com>
2015-01-17 15:11:39 -0600

    Poncho calling someone else

--- src/org/usfirst/frc/team2220/robot/BTTelephone.java
+++ src/org/usfirst/frc/team2220/robot/BTTelephone.java
@@ -2,5 +2,6 @@ package org.usfirst.frc.team2220.robot;
 
 public class BTTelephone {
 
-	//This can only end well. ~JE, calling Poncho
+	// This can only end well. ~JE, calling Poncho
+	// Poncho answers. Hi, this is Poncho, calling Saumik. You have 2 minutes to respond. Go.
 }
