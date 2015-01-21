c18eb9d93f71ebe306a3cc0a1cf8b30ab16c9658
benpankow <pankow.ben@gmail.com>
2015-01-20 19:14:10 -0600

    Cleaned up unused code

--- src/org/usfirst/frc/team2220/robot/drivetrain/BTIDrivetrain.java
+++ src/org/usfirst/frc/team2220/robot/drivetrain/BTIDrivetrain.java
@@ -1,5 +1,5 @@
-	package org.usfirst.frc.team2220.robot.drivetrain;
-//Test comment by Jacob E. - 1/17/15, 2:51 PM
+package org.usfirst.frc.team2220.robot.drivetrain;
+
 public interface BTIDrivetrain
 {
 	public void drive();
