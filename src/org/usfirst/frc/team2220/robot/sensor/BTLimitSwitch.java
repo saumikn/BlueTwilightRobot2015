c2d351007b177033bffcc5245b294d51cbdb50a5
benpankow <pankow.ben@gmail.com>
2015-01-26 20:12:54 -0600

    Added BTLimitSwitch

---
+++ src/org/usfirst/frc/team2220/robot/sensor/BTLimitSwitch.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team2220.robot.sensor;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+public class BTLimitSwitch extends DigitalInput
+{
+
+	public BTLimitSwitch(int channel) {
+		super(channel);
+	}
+
+}
